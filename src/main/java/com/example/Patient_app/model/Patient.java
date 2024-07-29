51d65766cddf96de114e9e38482de5493c01d6df
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-29 16:58:35 +0500

    add lombok

--- src/main/java/com/example/Patient_app/model/Patient.java
+++ src/main/java/com/example/Patient_app/model/Patient.java
@@ -1,11 +1,18 @@
 package com.example.Patient_app.model;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
 import java.time.LocalDate;
 
+@Data
+@Builder
 public class Patient {
     private String firstName;
     private String lastName;
     private LocalDate dateBirth;
+    @NonNull
     private String medCard;
     private int age;
 }
