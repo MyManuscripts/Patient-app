833aa74485f26b61eb7a3749d687a7f5099b9b6f
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-29 16:52:15 +0500

    create model Patient

---
+++ src/main/java/com/example/Patient_app/model/Patient.java
@@ -0,0 +1,11 @@
+package com.example.Patient_app.model;
+
+import java.time.LocalDate;
+
+public class Patient {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateBirth;
+    private String medCard;
+    private int age;
+}
