69a32f56d700d2aa41eca7f970baa25e8848e30f
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-30 16:04:02 +0500

    @Transactional

--- src/main/java/com/example/Patient_app/impl/PatientServiceImpl.java
+++ src/main/java/com/example/Patient_app/impl/PatientServiceImpl.java
@@ -1,35 +1,44 @@
 package com.example.Patient_app.impl;
 
 import com.example.Patient_app.model.Patient;
+import com.example.Patient_app.repository.PatientRepository;
 import com.example.Patient_app.service.PatientService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
+@Primary
 public class PatientServiceImpl implements PatientService {
+
+    private final PatientRepository repository;
     @Override
     public List<Patient> findAllPatients() {
-        return null;
+        return repository.findAll();
     }
 
     @Override
     public Patient savePatient(Patient patient) {
-        return null;
+        return repository.save(patient);
     }
 
     @Override
     public Patient findByMedCard(String medCard) {
-        return null;
+        return repository.findByMedCard(medCard);
     }
 
     @Override
     public Patient updatePatient(Patient patient) {
-        return null;
+        return repository.save(patient);
     }
 
     @Override
+    @Transactional
     public void deletePatient(String medCard) {
-
+        repository.deleteByMedCard(medCard);
     }
 }
