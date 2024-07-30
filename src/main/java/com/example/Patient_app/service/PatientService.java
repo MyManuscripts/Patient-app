6e2334aab1b5964b3e2ca78db27fb238a570934a
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-30 10:53:22 +0500

    create service

---
+++ src/main/java/com/example/Patient_app/service/PatientService.java
@@ -0,0 +1,11 @@
+package com.example.Patient_app.service;
+
+import com.example.Patient_app.model.Patient;
+
+import java.util.List;
+
+public class PatientService {
+    public List<Patient> findAllPatients(){
+        return null;
+    }
+}
