d51d6fcbea704875713891619faaddaa5a646462
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-30 12:09:20 +0500

    update methods

--- src/main/java/com/example/Patient_app/impl/InMemoryPatientServiceImpl.java
+++ src/main/java/com/example/Patient_app/impl/InMemoryPatientServiceImpl.java
@@ -1,35 +1,40 @@
 package com.example.Patient_app.impl;
 
 import com.example.Patient_app.model.Patient;
+import com.example.Patient_app.repository.InMemoryPatientDAO;
 import com.example.Patient_app.service.PatientService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@AllArgsConstructor
 public class InMemoryPatientServiceImpl implements PatientService {
+
+    private final InMemoryPatientDAO repository;
     @Override
     public List<Patient> findAllPatients() {
-        return null;
+        return repository.findAllPatients();
     }
 
     @Override
     public Patient savePatient(Patient patient) {
-        return null;
+        return repository.savePatient(patient);
     }
 
     @Override
     public Patient findByMedCard(String medCard) {
-        return null;
+        return repository.findByMedCard(medCard);
     }
 
     @Override
     public Patient updatePatient(Patient patient) {
-        return null;
+        return repository.updatePatient(patient);
     }
 
     @Override
 
     public void deletePatient(String medCard) {
-
+        repository.deletePatient(medCard);
     }
 }
