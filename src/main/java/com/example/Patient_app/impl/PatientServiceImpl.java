4f9b7f1c3671a0e833798c7d75482af5fbf55277
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-30 15:42:54 +0500

    extension service

---
+++ src/main/java/com/example/Patient_app/impl/PatientServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.Patient_app.impl;
+
+import com.example.Patient_app.model.Patient;
+import com.example.Patient_app.service.PatientService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+    @Override
+    public List<Patient> findAllPatients() {
+        return null;
+    }
+
+    @Override
+    public Patient savePatient(Patient patient) {
+        return null;
+    }
+
+    @Override
+    public Patient findByMedCard(String medCard) {
+        return null;
+    }
+
+    @Override
+    public Patient updatePatient(Patient patient) {
+        return null;
+    }
+
+    @Override
+    public void deletePatient(String medCard) {
+
+    }
+}
