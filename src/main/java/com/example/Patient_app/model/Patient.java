5c2b81b8810c3d480e16aeccf3e5239d660571e3
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-30 15:56:34 +0500

    getAge

--- src/main/java/com/example/Patient_app/model/Patient.java
+++ src/main/java/com/example/Patient_app/model/Patient.java
@@ -1,18 +1,31 @@
 package com.example.Patient_app.model;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Data
-@Builder
+// @Builder
+@Entity
 public class Patient {
+    @Id
+    @GeneratedValue
+    private long id;
     private String firstName;
     private String lastName;
     private LocalDate dateBirth;
-    @NonNull
+    @Column(unique = true)
     private String medCard;
+    @Transient
     private int age;
+
+    public int getAge() {
+        return Period.between(dateBirth,LocalDate.now()).getYears();
+    }
 }
+
