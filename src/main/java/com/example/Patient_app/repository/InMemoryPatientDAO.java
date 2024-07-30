761627354bd793e50d6eb508de792a3cc1ed4ac7
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-30 12:02:14 +0500

    DAO

---
+++ src/main/java/com/example/Patient_app/repository/InMemoryPatientDAO.java
@@ -0,0 +1,52 @@
+package com.example.Patient_app.repository;
+
+import com.example.Patient_app.model.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class InMemoryPatientDAO {
+    private final List<Patient>bufferListPatients = new ArrayList<>();
+
+
+    public List<Patient> findAllPatients() {
+        return bufferListPatients;
+    }
+
+
+    public Patient savePatient(Patient patient) {
+        bufferListPatients.add(patient);
+        return patient;
+    }
+
+
+    public Patient findByMedCard(String medCard) {
+        return bufferListPatients.stream()
+                .filter(e->e.getMedCard().equals(medCard))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    public Patient updatePatient(Patient patient) {
+        var patientIndex = IntStream.range(0,bufferListPatients.size())
+                .filter(index->bufferListPatients.get(index).getMedCard().equals(patient.getMedCard()))
+                .findFirst()
+                .orElse(-1);
+        if (patientIndex > -1){
+            bufferListPatients.add(patientIndex,patient);
+            return patient;
+        }
+        return null;
+    }
+
+
+
+    public void deletePatient(String medCard) {
+        var patient = findByMedCard(medCard);
+        if(patient != null){
+            bufferListPatients.remove(patient);
+        }
+    }
+}
