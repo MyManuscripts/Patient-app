00940d9a21675ee484706bc4f808aec81e601e26
DmitryKulikov-it <161183379+DmitryKulikov-it@users.noreply.github.com>
2024-07-30 12:28:10 +0500

    Play and testing in Postman

--- src/main/java/com/example/Patient_app/controller/PatientController.java
+++ src/main/java/com/example/Patient_app/controller/PatientController.java
@@ -1,16 +1,44 @@
 package com.example.Patient_app.controller;
 
 import com.example.Patient_app.model.Patient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Patient_app.service.PatientService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/patients")
+@AllArgsConstructor
+
 public class PatientController {
+
+    private PatientService service;
+
+    @GetMapping
     public List<Patient>findAllPatients(){
-        return null;
+        return service.findAllPatients();
+    }
+
+    @PostMapping("save_patient")
+    public String savePatient(@RequestBody Patient patient){
+        service.savePatient(patient);
+        return "patient saved";
+    }
+
+    @GetMapping("/{medCard}")
+    public Patient findByMedCard(@PathVariable String medCard){
+        return service.findByMedCard(medCard);
+    }
+
+    @PutMapping("update_patient")
+    public Patient updatePatient(@RequestBody Patient patient){
+        return service.updatePatient(patient);
+    }
+
+    @DeleteMapping("delete_patient")
+    void deletePatient(@PathVariable String medCard){
+        service.deletePatient(medCard);
     }
 
 }
